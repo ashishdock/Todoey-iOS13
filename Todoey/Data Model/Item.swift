839ca34c67fab41f1b471127db65005b803b2d9a
Ashish Sharma <94269501+ashishdock@users.noreply.github.com>
2022-12-17 17:39:37 +0530

    Refactored for fixing table view and database

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Ashish Sharma on 12/17/2022.
+//  Copyright © 2022 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
